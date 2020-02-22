eb9ea3b6c97934e3b8d49a734a50dfa0544ffa03
Pablo Ramirez <pablo@dreamsengine.io>
2020-02-21 19:51:41 -0600

    CRUD user

--- server/resolvers/Mutation/UserMutation.js
+++ server/resolvers/Mutation/UserMutation.js
@@ -3,5 +3,13 @@ const Users = require('../../models/user')
 module.exports = {
     createUser:(root,args) => {
         return Users.create(args.data)
+    },
+
+    updateUser:(root,args) => {
+        return Users.findByIdAndUpdate(args.id,{$set:{...args.data}},{new:true})
+    },
+
+    deleteUser:(root,args) => {
+        return Users.findByIdAndUpdate(args.id, {$set:{is_active:false}},{new:true})
     }
 }
